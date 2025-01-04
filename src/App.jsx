1aa312e3e1c45ae53ac15e83da364eab2f86b9ae
Aqib Shabir <aqibfaisalshabir@gmail.com>
2025-01-04 09:17:00 +0000

    added: regex validation for text inputs to only recieve number values and optional decimal values (currently styled in mobile - will make it responsive in future commits)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import Currency from './components/Currency';
-import { HiOutlineSwitchHorizontal } from 'react-icons/hi';
+import { HiOutlineSwitchHorizontal, HiOutlineSwitchVertical } from 'react-icons/hi';
 
 function App() {
   return (
     <>
-      <div className="bg-green-900 p-20">
+      <div className="bg-green-900 p-4 md:p-20">
         <div className="mb-4">
           <h1 className="text-6xl font-black text-green-400 mb-4">currency converter</h1>
           <h2 className="text-2xl text-green-400">convert from one currency to another</h2>
         </div>
         <div className="bg-white p-12 rounded-2xl">
-          <div className="flex justify-evenly items-center">
+          <div className="flex flex-col items-center md:flex md:flex-row md:justify-evenly md:items-center">
             <Currency label={'Amount'} country={'uk'} currencyValue={'GBP'} />
             <button className="mt-4">
-              <HiOutlineSwitchHorizontal size={30} />
+              <HiOutlineSwitchVertical size={30} className="m-4" />
             </button>
             <Currency label={'Converted to'} country={'us'} currencyValue={'USD'} />
           </div>
--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import { CircleFlag } from 'react-circle-flags';
 import { FaAngleDown } from 'react-icons/fa';
 
 function Currency({ label, country, currencyValue }) {
+  const [currentValue, setCurrentValue] = useState('');
+
+  const checkValue = (e) => {
+    setCurrentValue(handleDecimalsOnValue(e.target.value));
+  };
+
+  const handleDecimalsOnValue = (value) => {
+    const regex = /\d*\.?\d{0,2}/;
+    return value.match(regex)[0];
+  };
+
   return (
     <div className="flex flex-col">
-      <label htmlFor="" className="text-gray-500 text-sm mb-2 font-light">
-        {label}
-      </label>
+      <p className="text-gray-500 text-sm mb-2 font-light">{label}</p>
       <div className="border border-black rounded-md flex">
-        <input type="text" className="rounded-md p-4 font-semibold text-xl" />
-        <button className="rounded-md p-4">
+        <input
+          type="text"
+          value={currentValue}
+          onChange={checkValue}
+          className="rounded-md p-4 w-[9.5rem] font-semibold text-lg sm:text-xl"
+        />
+        <button className="rounded-md p-4" onClick={() => console.log('show all currencies')}>
           <span className="flex items-center justify-between">
             <span className="mr-2">
-              <CircleFlag countryCode={country} width={35} />
+              <CircleFlag countryCode={country} className="max-w-8" />
             </span>
             <span className="font-semibold mr-2 text-lg">{currencyValue}</span>
             <span>
